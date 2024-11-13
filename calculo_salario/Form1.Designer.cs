83bf61b1ec59c1fe550581ad788f894747918782
jpbertti <joaobertty@gmail.com>
2024-11-13 11:58:53 -0300

    Adicionar arquivos de projeto.

---
+++ calculo_salario/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace calculo_salario
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSair = new System.Windows.Forms.Button();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblValorHora = new System.Windows.Forms.Label();
+            this.lblHorasTrabalhadas = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtValorHora = new System.Windows.Forms.TextBox();
+            this.txtHorasTrabalhadas = new System.Windows.Forms.TextBox();
+            this.lblMensal = new System.Windows.Forms.Label();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSair
+            // 
+            this.btnSair.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSair.Location = new System.Drawing.Point(23, 297);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(75, 25);
+            this.btnSair.TabIndex = 0;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.Location = new System.Drawing.Point(253, 297);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(88, 25);
+            this.btnCalcular.TabIndex = 1;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(71, 43);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(224, 18);
+            this.lblTitulo.TabIndex = 2;
+            this.lblTitulo.Text = "Informe os valores a seguir: ";
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNome.Location = new System.Drawing.Point(53, 99);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(58, 18);
+            this.lblNome.TabIndex = 3;
+            this.lblNome.Text = "Nome:";
+            // 
+            // lblValorHora
+            // 
+            this.lblValorHora.AutoSize = true;
+            this.lblValorHora.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblValorHora.Location = new System.Drawing.Point(53, 157);
+            this.lblValorHora.Name = "lblValorHora";
+            this.lblValorHora.Size = new System.Drawing.Size(99, 18);
+            this.lblValorHora.TabIndex = 4;
+            this.lblValorHora.Text = "Valor Hora: ";
+            // 
+            // lblHorasTrabalhadas
+            // 
+            this.lblHorasTrabalhadas.AutoSize = true;
+            this.lblHorasTrabalhadas.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHorasTrabalhadas.Location = new System.Drawing.Point(53, 216);
+            this.lblHorasTrabalhadas.Name = "lblHorasTrabalhadas";
+            this.lblHorasTrabalhadas.Size = new System.Drawing.Size(161, 18);
+            this.lblHorasTrabalhadas.TabIndex = 5;
+            this.lblHorasTrabalhadas.Text = "Horas Trabalhadas: ";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(133, 99);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(196, 20);
+            this.txtNome.TabIndex = 6;
+            // 
+            // txtValorHora
+            // 
+            this.txtValorHora.Location = new System.Drawing.Point(158, 155);
+            this.txtValorHora.Name = "txtValorHora";
+            this.txtValorHora.Size = new System.Drawing.Size(171, 20);
+            this.txtValorHora.TabIndex = 7;
+            // 
+            // txtHorasTrabalhadas
+            // 
+            this.txtHorasTrabalhadas.Location = new System.Drawing.Point(211, 214);
+            this.txtHorasTrabalhadas.Name = "txtHorasTrabalhadas";
+            this.txtHorasTrabalhadas.Size = new System.Drawing.Size(118, 20);
+            this.txtHorasTrabalhadas.TabIndex = 8;
+            // 
+            // lblMensal
+            // 
+            this.lblMensal.AutoSize = true;
+            this.lblMensal.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMensal.Location = new System.Drawing.Point(53, 234);
+            this.lblMensal.Name = "lblMensal";
+            this.lblMensal.Size = new System.Drawing.Size(56, 18);
+            this.lblMensal.TabIndex = 9;
+            this.lblMensal.Text = "Mensal";
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpar.Location = new System.Drawing.Point(139, 297);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(75, 25);
+            this.btnLimpar.TabIndex = 10;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(375, 334);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.lblMensal);
+            this.Controls.Add(this.txtHorasTrabalhadas);
+            this.Controls.Add(this.txtValorHora);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.lblHorasTrabalhadas);
+            this.Controls.Add(this.lblValorHora);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.btnSair);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label lblValorHora;
+        private System.Windows.Forms.Label lblHorasTrabalhadas;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtValorHora;
+        private System.Windows.Forms.TextBox txtHorasTrabalhadas;
+        private System.Windows.Forms.Label lblMensal;
+        private System.Windows.Forms.Button btnLimpar;
+    }
+}
+
---
+++ calculo_salario/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculo_salario
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show ("Deseja realmente sair?", "Sair", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                Close();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            string Nome;                                                          //inicialização com 0 quando é menor de 1440
+            double valor_hora, horas_trabalhadas, salario_bruto, vale_transporte, imposto_de_renda = 0, salario_liquido;
+            
+            Nome = txtNome.Text;
+            valor_hora = double.Parse(txtValorHora.Text);
+            horas_trabalhadas = double.Parse(txtHorasTrabalhadas.Text);
+
+            salario_bruto = valor_hora * horas_trabalhadas;
+
+            vale_transporte = salario_bruto * 0.06;
+
+            if (salario_bruto > 1440)
+            {
+                imposto_de_renda = salario_bruto * 0.11;
+            }
+            // else
+            //{
+               // imposto_de_renda = 0;
+           // }
+
+            salario_liquido = salario_bruto - vale_transporte - imposto_de_renda;
+
+            MessageBox.Show("O Salario bruto do(a) " + Nome + " é de: " + salario_bruto.ToString("C"));
+
+            MessageBox.Show("O valor do Vale Transporte é de: " + vale_transporte.ToString("C"));
+
+            MessageBox.Show("O desconto do imposto de renda é de: " + imposto_de_renda.ToString("C"));
+
+            MessageBox.Show("O Valor do Salário líquido é: " + salario_liquido.ToString("C"));
+
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtHorasTrabalhadas.Clear();
+            txtNome.Clear();
+            txtValorHora.Clear();
+            txtNome.Focus();
+        }
+    }
+}
